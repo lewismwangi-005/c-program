a54df5b2d08b66c6d2907b00b5275a22cbd8e477
lewismwangi-005 <lewiswangai45@gmail.com>
2026-09-15 23:42:00 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//units offered this semester
+#include <stdio.h>
+
+int main(void) {
+    printf("units offered this semester\n");
+    printf("introduction to programming\n");
+    printf("data structures\n");
+    printf("computer networks\n");
+    printf("database systems\n");
+    printf("software engeneering\n");
+
+
+    return 0;
+}
